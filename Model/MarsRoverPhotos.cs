3be76a2302c6031a7830f696f2ed96ec439a0629
Vlad <vladp.ua777@gmail.com>
2022-06-20 22:33:08 +0300

    Add MarsRoverPhotos

---
+++ Model/MarsRoverPhotos.cs
@@ -0,0 +1,27 @@
+﻿namespace NasaApi.Model
+{
+    public class MarsRoverPhotos
+    {
+        public List<Photos> photos { get; set; }
+    }
+    public class Photos
+    {
+        public Camera camera { get; set; }
+        public string img_src { get; set; }
+        public string earth_date { get; set; }
+
+    }
+    public class Camera
+    {
+        public string name { get; set; }
+        public string full_name { get; set; }
+    }
+    public class Rover
+    {
+        public string name { get; set; }
+        public string landing_date { get; set; }
+        public string launch_date { get; set; }
+        public string status { get; set; }
+
+    }
+}
